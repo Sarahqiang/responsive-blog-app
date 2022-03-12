8b8d203c2dae05c16646c46e72d9e46aef6112ba
lukewu <70121737+Sarahqiang@users.noreply.github.com>
2022-03-12 15:25:08 -0500

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,6 @@
+I use chrome and i made this web page responsive for three size which are 
+1.Android (Pixel 2) portrait · width: 412px(phone size)
+2.iPhone 6-8 Plump portrait · width: 414px 
+3.MacBook Pro 2018 (13 inch) width 1600px
+
+https://www.cs.mcgill.ca/~ywu151/mini5/blog5.html
---
+++ script.js
@@ -0,0 +1,26 @@
+  
+
+    function changeColor() {
+        document.getElementById("change1").style.backgroundColor ="#FF3333";
+        document.getElementById("change2").style.backgroundColor="#FF5733";
+        document.getElementById("change3").style.backgroundColor="#FF7733";
+        document.getElementById("change5").style.color="#FF5733";
+        document.getElementById("change6").style.color="#FF5733";
+        document.getElementById("change7").style.color="#FF5733";
+        document.getElementById("change8").style.color="#FF5733";
+        document.getElementById("change9").style.color="#FF5733";
+        document.getElementById("change10").style.color="#FF5733";
+        document.getElementById("change11").style.color="#FF5733";
+        document.getElementById("change12").style.color="#FF5733";
+        document.getElementById("change13").style.color="#FF5733";
+        document.getElementById("change14").style.backgroundColor="#FF5733";
+        document.getElementById("change15").style.color="#FF5733";
+        document.getElementById("change16").style.color="#FF5733";
+        document.getElementById("change17").style.backgroundColor="#FF5733";
+        document.getElementById("change18").style.backgroundColor="#FF3333";
+        document.getElementById("change19").style.backgroundColor="#FF3333";
+        
+        
+        
+        
+    }   
